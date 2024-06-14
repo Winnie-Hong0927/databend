0387407edd9eb7020c1d3fce139f2020c51598c4
Winnie-Hong0927 <136137323+Winnie-Hong0927@users.noreply.github.com>
2024-06-14 20:20:44 +0800

    Explain bench.rs
    This file is mainly to test the performance of SQL parser defined in databend_common_ast crate when it is used to process different types and different scales of SQL statements.By using `cirterion` crate,developer can understand the bottleneck of the performance of the SQL parsers  and optimize SQL parsers accordingly.

--- src/query/ast/benches/bench.rs
+++ src/query/ast/benches/bench.rs
@@ -12,25 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//导入 criterion 库，并启用宏。
 #[macro_use]
 extern crate criterion;
-
+// 从 criterion 库中导入所需的函数和类型。
 use criterion::black_box;
 use criterion::Criterion;
+// 从 databend_common_ast 库中导入 SQL 解析相关的函数和类型。
 use databend_common_ast::parser::parse_expr;
 use databend_common_ast::parser::parse_sql;
 use databend_common_ast::parser::tokenize_sql;
 use databend_common_ast::parser::Dialect;
-
+// 定义一个基准测试函数，接受一个 Criterion 类型的可变引用作为参数。
 fn bench(c: &mut Criterion) {
+    // 创建一个基准测试组，命名为 bench_parser。
     let mut group = c.benchmark_group("bench_parser");
+    // 设置基准测试组的样本大小为 10（即每个测试运行 10 次）。
     group.sample_size(10);
-
+// 定义一个名为 large_statement 的基准测试函数。
     group.bench_function("large_statement", |b| {
+        // 在每次迭代中运行以下代码。
         b.iter(|| {
+            // 定义一个复杂的 SQL 语句。
             let case = r#"explain SELECT SUM(count) FROM (SELECT ((((((((((((true)and(true)))or((('614')like('998831')))))or(false)))and((true IN (true, true, (-1014651046 NOT BETWEEN -1098711288 AND -1158262473))))))or((('780820706')=('')))) IS NOT NULL AND ((((((((((true)AND(true)))or((('614')like('998831')))))or(false)))and((true IN (true, true, (-1014651046 NOT BETWEEN -1098711288 AND -1158262473))))))OR((('780820706')=(''))))) ::INT64)as count FROM t0) as res;"#;
+            // 将 SQL 语句转换为 tokens（词法分析）
             let tokens = tokenize_sql(case).unwrap();
+            // 解析 tokens，生成 SQL 抽象语法树（AST）。
             let (stmt, _) = parse_sql(&tokens, Dialect::PostgreSQL).unwrap();
+            // 用 black_box 来防止编译器对未使用结果的优化。
             black_box(stmt);
         })
     });
@@ -67,6 +76,7 @@ fn bench(c: &mut Criterion) {
         })
     });
 }
-
+// 将定义的基准测试函数 bench 包装成一个基准测试组 benches。
 criterion_group!(benches, bench);
+// criterion_main!(benches);：定义程序的入口点，运行所有基准测试组。
 criterion_main!(benches);
